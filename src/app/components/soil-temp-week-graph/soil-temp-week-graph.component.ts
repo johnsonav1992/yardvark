1fe40cee652179894349e2556391a7a7837fd702
Alex Johnson <johnsonav1992@gmail.com>
2025-03-07 08:18:07 -0600

    Add chart loader component and update soil temperature graph for loading state

--- src/app/components/soil-temp-week-graph/soil-temp-week-graph.component.ts
+++ src/app/components/soil-temp-week-graph/soil-temp-week-graph.component.ts
@@ -4,12 +4,13 @@ import { ChartModule } from 'primeng/chart';
 import { getFullWeekOfDayLabelsCenteredAroundCurrentDay } from '../../utils/timeUtils';
 import { OpenMeteoQueryParams } from '../../types/openmeteo.types';
 import { getPrimeNgHexColor } from '../../utils/styleUtils';
+import { ChartLoaderComponent } from '../chart-loader/chart-loader.component';
 
 @Component({
   selector: 'soil-temp-week-graph',
-  imports: [ChartModule],
+  imports: [ChartModule, ChartLoaderComponent],
   templateUrl: './soil-temp-week-graph.component.html',
-  styleUrl: './soil-temp-week-graph.component.scss',
+  styleUrl: './soil-temp-week-graph.component.scss'
 })
 export class SoilTempWeekGraphComponent {
   public dailyAverageShallowTemps = input.required<number[]>();
@@ -19,13 +20,13 @@ export class SoilTempWeekGraphComponent {
   public isLoadingChartData = input<boolean>(false);
 
   public displayTempUnit = computed(
-    () => `${this.tempUnit() === 'fahrenheit' ? '°F' : '°C'}`,
+    () => `${this.tempUnit() === 'fahrenheit' ? '°F' : '°C'}`
   );
 
   public tempsChartData = computed<ChartData<'line'>>(() => ({
     labels: getFullWeekOfDayLabelsCenteredAroundCurrentDay({
       includeDates: true,
-      shortDayNames: true,
+      shortDayNames: true
     }),
     datasets: [
       {
@@ -33,16 +34,16 @@ export class SoilTempWeekGraphComponent {
         label: `3in. depth ${this.displayTempUnit()}`,
         data: this.dailyAverageShallowTemps(),
         borderColor: getPrimeNgHexColor('indigo.400'),
-        tension: 0.4,
+        tension: 0.4
       },
       {
         type: 'line',
         label: `7in. depth ${this.displayTempUnit()}`,
         data: this.dailyAverageDeepTemps(),
         borderColor: getPrimeNgHexColor('lime.400'),
-        tension: 0.4,
-      },
-    ],
+        tension: 0.4
+      }
+    ]
   }));
 
   public options: ChartOptions<'line'> = {
@@ -54,15 +55,15 @@ export class SoilTempWeekGraphComponent {
         min: 0,
         max: 100,
         ticks: {
-          stepSize: 20,
-        },
-      },
+          stepSize: 20
+        }
+      }
     },
     plugins: {
       legend: {
         position: 'chartArea',
-        align: 'end',
-      },
-    },
+        align: 'end'
+      }
+    }
   };
 }

48a4bde0bca06e0c8312fbd295e1a4e8d815cdd6
Alex Johnson <johnsonav1992@gmail.com>
2025-03-08 06:39:55 -0600

    Update settings component to support temperature unit selection; refactor HTTP utilities for dynamic URL construction and add utility functions

--- src/app/utils/httpUtils.ts
+++ src/app/utils/httpUtils.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import {
   ApplicationRef,
   EnvironmentInjector,
+  inject,
   runInInjectionContext
 } from '@angular/core';
 import { EndPoints } from '../types/endpoints.types';
@@ -9,6 +10,11 @@ import { BE_URL } from '../constants/api-constants';
 
 let environmentInjector: EnvironmentInjector | null = null;
 
+/**
+ * Initializes the HTTP utilities by injecting the necessary dependencies.
+ *
+ * @param appRef - The reference to the application, used to access the injector.
+ */
 export const initHttpUtils = (appRef: ApplicationRef) => {
   environmentInjector = appRef.injector.get(EnvironmentInjector);
 };
@@ -55,4 +61,37 @@ export const deleteReq = createHttpUtil(
   }
 );
 
-export const beUrl = (path: EndPoints) => `${BE_URL}/${path}`;
+/**
+ * Constructs a full backend URL by appending the given endpoint path to the base URL.
+ *
+ * @param path - The endpoint path to be appended to the base URL.
+ * @param opts - Optional parameters and query parameters to be included in the URL.
+ * @returns The full URL as a string.
+ */
+export const beUrl = (
+  path: EndPoints,
+  opts?: {
+    params?: Array<string | number>;
+    queryParams?: Record<string, unknown>;
+  }
+) => {
+  let url = `${BE_URL}/${path}`;
+
+  if (opts?.params) {
+    url += '/' + opts.params.join('/');
+  }
+
+  if (opts?.queryParams) {
+    const queryParams = new URLSearchParams(
+      opts.queryParams as Record<string, string>
+    ).toString();
+    url += `?${queryParams}`;
+  }
+
+  return url;
+};
+
+/**
+ * A quicker way to get the http client in a component or service
+ */
+export const injectHttp = () => inject(HttpClient);
--- src/app/views/settings/settings.component.ts
+++ src/app/views/settings/settings.component.ts
@@ -1,18 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, computed } from '@angular/core';
 import { PageContainerComponent } from '../../components/layout/page-container/page-container.component';
 import { httpResource } from '@angular/common/http';
 import { beUrl, putReq } from '../../utils/httpUtils';
+import { Settings } from '../../types/settings.types';
+import { DropdownModule } from 'primeng/dropdown';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'settings',
-  imports: [PageContainerComponent],
+  imports: [PageContainerComponent, DropdownModule, FormsModule],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.scss'
 })
 export class SettingsComponent {
-  public settings = httpResource(beUrl('settings'));
+  public settings = httpResource<Settings>(beUrl('settings'));
 
-  public updateSetting(settingName: string, newValue: string): void {
-    putReq(beUrl('settings'), { temperatureUnit: 'celcius' });
+  public currentSettings = computed(() => this.settings.value());
+
+  public updateSetting<
+    TKey extends keyof Settings,
+    TValue extends Settings[TKey]
+  >(settingName: TKey, newValue: TValue): void {
+    putReq(beUrl('settings'), {
+      ...this.currentSettings(),
+      [settingName]: newValue
+    }).subscribe();
   }
 }

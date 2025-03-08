a85c8262ed1581310acb0aefe1f6b136595fe9f7
Alex Johnson <johnsonav1992@gmail.com>
2025-03-08 06:10:05 -0600

    Update settings component to manage settings state and add update functionality; adjust TypeScript configuration for unused locals and parameters

--- src/app/views/settings/settings.component.ts
+++ src/app/views/settings/settings.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { PageContainerComponent } from '../../components/layout/page-container/page-container.component';
 import { httpResource } from '@angular/common/http';
-import { beUrl } from '../../utils/httpUtils';
+import { beUrl, putReq } from '../../utils/httpUtils';
 
 @Component({
   selector: 'settings',
@@ -10,5 +10,9 @@ import { beUrl } from '../../utils/httpUtils';
   styleUrl: './settings.component.scss'
 })
 export class SettingsComponent {
-  public posts = httpResource(beUrl('settings'));
+  public settings = httpResource(beUrl('settings'));
+
+  public updateSetting(settingName: string, newValue: string): void {
+    putReq(beUrl('settings'), { temperatureUnit: 'celcius' });
+  }
 }

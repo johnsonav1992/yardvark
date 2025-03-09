cf013c3c25629decda4d8b6763bdab7428177911
Alex Johnson <johnsonav1992@gmail.com>
2025-03-09 14:35:52 -0500

    setting up name change ui

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,21 +1,30 @@
-import { Component, effect } from '@angular/core';
+import { Component, linkedSignal, signal } from '@angular/core';
 import { PageContainerComponent } from '../../components/layout/page-container/page-container.component';
 import { injectUserData } from '../../utils/authUtils';
 import { AvatarModule } from 'primeng/avatar';
 import { AvatarDesignTokens } from '@primeng/themes/types/avatar';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'profile',
-  imports: [PageContainerComponent, AvatarModule],
+  imports: [
+    PageContainerComponent,
+    AvatarModule,
+    InputTextModule,
+    ButtonModule,
+    FormsModule
+  ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent {
   public user = injectUserData();
 
-  _ = effect(() => {
-    console.log(this.user());
-  });
+  public isEditingField = signal<'name' | 'email' | null>(null);
+
+  public name = linkedSignal(() => this.user()?.name);
 
   public avatarDt: AvatarDesignTokens = {
     root: {

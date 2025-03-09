d4fc95c3adad6fec6dcd53b060ecf27e9f7517a4
Alex Johnson <johnsonav1992@gmail.com>
2025-03-09 14:59:52 -0500

    trying to do user updates...

--- src/app/constants/api-constants.ts
+++ src/app/constants/api-constants.ts
@@ -1,1 +1,2 @@
 export const BE_API_URL = 'http://localhost:3000';
+export const AUTH0_USER_MANAGEMENT_URL = 'https://login.auth0.com/api/v2/users';
--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import { Component, linkedSignal, signal } from '@angular/core';
+import { Component, inject, linkedSignal, signal } from '@angular/core';
 import { PageContainerComponent } from '../../components/layout/page-container/page-container.component';
 import { injectUserData } from '../../utils/authUtils';
 import { AvatarModule } from 'primeng/avatar';
@@ -6,6 +6,9 @@ import { AvatarDesignTokens } from '@primeng/themes/types/avatar';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { FormsModule } from '@angular/forms';
+import { AUTH0_USER_MANAGEMENT_URL } from '../../constants/api-constants';
+import { postReq } from '../../utils/httpUtils';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'profile',
@@ -20,11 +23,41 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent {
+  private _auth = inject(AuthService);
+
   public user = injectUserData();
 
   public isEditingField = signal<'name' | 'email' | null>(null);
 
   public name = linkedSignal(() => this.user()?.name);
+
+  public updateName(): void {
+    const data = JSON.stringify({
+      name: this.name()
+    });
+
+    this._auth.getAccessTokenSilently().subscribe({
+      next: (token) => {
+        console.log({ data });
+
+        const config = {
+          method: 'patch',
+          maxBodyLength: Infinity,
+          headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+            Authorization: `Bearer ${token}`
+          }
+        };
+
+        postReq(
+          `${AUTH0_USER_MANAGEMENT_URL}/${this.user()?.sub}`,
+          data,
+          config
+        );
+      }
+    });
+  }
 
   public avatarDt: AvatarDesignTokens = {
     root: {

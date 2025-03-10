3a1cecd6040bc955e58349bf1b7b0440f5a0d3ab
Alex Johnson <johnsonav1992@gmail.com>
2025-03-10 05:21:11 -0500

    db connected

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,25 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SettingsModule } from './settings/settings.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [SettingsModule],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.env' }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DEVPGHOST,
+      port: 5432,
+      username: process.env.DEVPGUSER,
+      password: process.env.DEVPGPASSWORD,
+      database: process.env.DEVPGDATABASE,
+      ssl: true,
+      entities: [],
+      synchronize: true,
+    }),
+    SettingsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

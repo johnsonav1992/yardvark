71f4a523e2a5b2c17598bcbed9f16f0b7fc69ca3
Alex Johnson <johnsonav1992@gmail.com>
2025-03-10 08:53:43 -0500

    more BE cleaning

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { SettingsModule } from './settings/settings.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
@@ -21,7 +19,7 @@ import { ConfigModule } from '@nestjs/config';
     }),
     SettingsModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}

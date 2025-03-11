3ae5f7b67f36448967a94483658ce0e588fc292f
Alex Johnson <johnsonav1992@gmail.com>
2025-03-11 04:58:56 -0500

    styling calendar and clean up ts file

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -2,20 +2,13 @@ import { DatePipe, NgTemplateOutlet } from '@angular/common';
 import {
   Component,
   computed,
+  contentChild,
   input,
   output,
   signal,
   TemplateRef
 } from '@angular/core';
-import {
-  addMonths,
-  eachDayOfInterval,
-  endOfMonth,
-  format,
-  startOfMonth,
-  startOfToday,
-  subMonths
-} from 'date-fns';
+import { addMonths, format, startOfToday, subMonths } from 'date-fns';
 import { getCalendarDaysData } from './utils';
 import { ButtonModule } from 'primeng/button';
 
@@ -27,13 +20,9 @@ import { ButtonModule } from 'primeng/button';
   imports: [DatePipe, NgTemplateOutlet, ButtonModule]
 })
 export class CalendarComponent {
-  public markers = input<CalendarMarkerData[], CalendarMarkerData[] | null>(
-    [],
-    {
-      transform: (value) => value ?? []
-    }
-  );
-  public markerTpl = input<TemplateRef<{ $implicit: CalendarMarkerData[] }>>();
+  public markers = input<CalendarMarkerData[]>([]);
+  public markerTpl =
+    contentChild<TemplateRef<{ $implicit: CalendarMarkerData[] }>>('marker');
 
   public monthChange = output<Date>();
 
@@ -42,65 +31,45 @@ export class CalendarComponent {
     format(this.currentDate(), 'MMMM yyyy')
   );
 
-  protected readonly startDateOfSelectedMonth = computed(() => {
-    return startOfMonth(this.currentDate());
-  });
-  protected readonly endDateOfSelectedMonth = computed(() => {
-    return endOfMonth(this.currentDate());
-  });
-
-  protected readonly days = computed(() => {
-    return eachDayOfInterval({
-      start: this.startDateOfSelectedMonth(),
-      end: this.endDateOfSelectedMonth()
-    });
-  });
-
-  readonly markersMap = computed(() => {
-    const map: Map<string, CalendarMarkerData[]> = new Map();
-    this.markers().forEach((marker) => {
-      const date = marker.date;
-      const markers = map.get(this.getMarkerMapKey(date)) || [];
-      markers.push(marker);
-      map.set(this.getMarkerMapKey(date), markers);
-    });
-    return map;
-  });
-
-  /**
-   * Returns the days enriched with grid offsets and markers
-   */
-  public daysEnriched = computed(() =>
+  public days = computed(() =>
     getCalendarDaysData(this.currentDate(), this.markers())
   );
 
-  readonly dayNames = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-  protected readonly dayNamesFormatted = this.dayNames.map((dayName) => ({
+  private readonly _dayNames = [
+    'Sun',
+    'Mon',
+    'Tue',
+    'Wed',
+    'Thu',
+    'Fri',
+    'Sat'
+  ];
+  public readonly dayNamesFormatted = this._dayNames.map((dayName) => ({
     dayName: dayName,
     isToday: dayName === format(startOfToday(), 'eee')
   }));
 
-  protected nextMonth() {
+  public nextMonth() {
     this.currentDate.set(addMonths(this.currentDate(), 1));
     this.monthChange.emit(this.currentDate());
   }
 
-  protected prevMonth() {
+  public prevMonth() {
     this.currentDate.set(subMonths(this.currentDate(), 1));
     this.monthChange.emit(this.currentDate());
   }
 
-  protected toCurrentMonth(): void {
+  public toCurrentMonth(): void {
     this.currentDate.set(startOfToday());
     this.monthChange.emit(this.currentDate());
   }
 
-  protected getMarkerMapKey(date: Date): string {
+  public getMarkerMapKey(date: Date): string {
     return format(date, 'yyyy-MM-dd');
   }
 }
 
-export interface CalendarMarkerData<Data = any> {
+export interface CalendarMarkerData<Data = unknown> {
   date: Date;
   data: Data;
 }

8b2143ce3a4f68fdb6310ca0c243793dfbf98b75
Alex Johnson <johnsonav1992@gmail.com>
2025-03-14 04:58:44 -0500

    feat: add back navigation button and improve title layout in page container

--- src/app/components/layout/page-container/page-container.component.ts
+++ src/app/components/layout/page-container/page-container.component.ts
@@ -1,11 +1,19 @@
-import { Component, input } from '@angular/core';
+import { Location } from '@angular/common';
+import { Component, inject, input } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'page-container',
-  imports: [],
+  imports: [ButtonModule],
   templateUrl: './page-container.component.html',
   styleUrl: './page-container.component.scss'
 })
 export class PageContainerComponent {
+  private _location = inject(Location);
+
   public pageTitle = input.required<string>();
+
+  public back() {
+    this._location.back();
+  }
 }

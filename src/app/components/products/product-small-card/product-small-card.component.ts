a81b2e39611fbfd3f00bb2429932563bc241c0e6
Alex Johnson <johnsonav1992@gmail.com>
2025-03-19 05:34:31 -0500

    feat: integrate product-small-card component for enhanced product display in entry dialogs

--- src/app/components/products/product-small-card/product-small-card.component.ts
+++ src/app/components/products/product-small-card/product-small-card.component.ts
@@ -1,9 +1,18 @@
-import { Component, ElementRef, inject, input, OnInit } from '@angular/core';
+import {
+  Component,
+  contentChild,
+  ElementRef,
+  inject,
+  input,
+  OnInit,
+  TemplateRef
+} from '@angular/core';
 import { Product } from '../../../types/products.types';
+import { NgTemplateOutlet } from '@angular/common';
 
 @Component({
   selector: 'product-small-card',
-  imports: [],
+  imports: [NgTemplateOutlet],
   templateUrl: './product-small-card.component.html',
   styleUrl: './product-small-card.component.scss'
 })
@@ -14,6 +23,8 @@ export class ProductSmallCardComponent implements OnInit {
   public fullWidth = input<boolean>(false);
   public showBorder = input<boolean>(true);
   public asAppliedAmount = input<boolean>(false);
+
+  public actions = contentChild<TemplateRef<unknown>>('actions');
 
   public ngOnInit(): void {
     if (this.fullWidth()) {

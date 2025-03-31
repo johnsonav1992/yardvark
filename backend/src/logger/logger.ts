ccc9baf8a5ed1d0d66b6b209211d5f5d0ee9605f
Alex Johnson <johnsonav1992@gmail.com>
2025-03-31 07:02:59 -0500

    feat: add logging interceptor for HTTP request logging in the application

---
+++ backend/src/logger/logger.ts
@@ -0,0 +1,42 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  Logger,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+@Injectable()
+export class LoggingInterceptor implements NestInterceptor {
+  private logger = new Logger('HTTP');
+
+  intercept(context: ExecutionContext, next: CallHandler): Observable<unknown> {
+    const httpContext = context.switchToHttp();
+    const request = httpContext.getRequest<{
+      method: string;
+      url: string;
+      body: unknown;
+    }>();
+    const response = httpContext.getResponse<{ statusCode: number }>();
+
+    const { method, url, body } = request;
+    const start = Date.now();
+
+    return next.handle().pipe(
+      tap(() => {
+        const duration = Date.now() - start;
+        const statusCode = response.statusCode;
+
+        let logMessage = `${method} ${url} ${statusCode} - ${duration}ms`;
+
+        if (body) {
+          logMessage += ` - ${JSON.stringify(body, null, 2)}`;
+        }
+
+        this.logger.log(logMessage);
+      }),
+    );
+  }
+}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,11 @@
 /* eslint-disable @typescript-eslint/no-floating-promises */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { LoggingInterceptor } from './logger/logger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new LoggingInterceptor());
   app.enableCors();
 
   await app.listen(process.env.PORT ?? 8080);

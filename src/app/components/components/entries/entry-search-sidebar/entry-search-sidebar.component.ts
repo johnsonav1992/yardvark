ed2d58b9f8ff0d34c8354f5831f45319309c0812
Alex Johnson <johnsonav1992@gmail.com>
2025-04-15 05:41:29 -0500

    date input

--- src/app/components/components/entries/entry-search-sidebar/entry-search-sidebar.component.ts
+++ src/app/components/components/entries/entry-search-sidebar/entry-search-sidebar.component.ts
@@ -9,6 +9,8 @@ import { ActivitiesService } from '../../../../services/activities.service';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { LawnSegmentsService } from '../../../../services/lawn-segments.service';
 import { ProductsService } from '../../../../services/products.service';
+import { GlobalUiService } from '../../../../services/global-ui.service';
+import { DatePickerModule } from 'primeng/datepicker';
 
 @Component({
   selector: 'entry-search-sidebar',
@@ -19,7 +21,8 @@ import { ProductsService } from '../../../../services/products.service';
     DrawerModule,
     DividerModule,
     MultiSelectModule,
-    FloatLabelModule
+    FloatLabelModule,
+    DatePickerModule
   ],
   templateUrl: './entry-search-sidebar.component.html',
   styleUrl: './entry-search-sidebar.component.scss'
@@ -28,10 +31,13 @@ export class EntrySearchSidebarComponent {
   private _activitiesService = inject(ActivitiesService);
   private _lawnSegmentsService = inject(LawnSegmentsService);
   private _productsService = inject(ProductsService);
+  private _globalUiService = inject(GlobalUiService);
 
   public isOpen = model(false);
 
   public activities = this._activitiesService.activities;
   public lawnSegments = this._lawnSegmentsService.lawnSegments;
   public products = this._productsService.products;
+
+  public isMobile = this._globalUiService.isMobile;
 }

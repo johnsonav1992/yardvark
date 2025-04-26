ebdebc523ca5e73699a9271eb1bfcdcb27389347
Alex Johnson <johnsonav1992@gmail.com>
2025-04-26 05:18:04 -0500

    page is working

--- src/app/pages/products/product-view/product-view.component.ts
+++ src/app/pages/products/product-view/product-view.component.ts
@@ -1,11 +1,9 @@
-import { Component, computed, inject, input } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { PageContainerComponent } from '../../../components/layout/page-container/page-container.component';
-import { Product } from '../../../types/products.types';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { ActivatedRoute } from '@angular/router';
 import { map } from 'rxjs';
 import { ProductsService } from '../../../services/products.service';
-import { effectSignalLogger } from '../../../utils/generalUtils';
 
 @Component({
   selector: 'product-view',
@@ -17,16 +15,17 @@ export class ProductViewComponent {
   private _route = inject(ActivatedRoute);
   private _productsService = inject(ProductsService);
 
-  public productId = toSignal<number>(
-    this._route.params.pipe(map((params) => params['productId']))
+  public productId = toSignal(
+    this._route.params.pipe(map((params) => parseInt(params['productId'])))
   );
 
-  public product = computed(
-    () =>
-      this._productsService.products
-        .value()
-        ?.find((product) => product.id === this.productId())!
+  public product = computed(() =>
+    this._productsService.products
+      .value()
+      ?.find((product) => product.id === this.productId())
   );
 
-  _ = effectSignalLogger(this.product);
+  public pageTitle = computed(
+    () => `${this.product()?.brand} - ${this.product()?.name}`
+  );
 }

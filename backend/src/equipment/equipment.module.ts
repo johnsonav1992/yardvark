dc0553ded44dfd121d5418b0027c8c7ab1ec26da
Alex Johnson <johnsonav1992@gmail.com>
2025-04-27 05:21:26 -0500

    spelling...

--- backend/src/equipment/equipment.module.ts
+++ backend/src/equipment/equipment.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { EquipmentController } from './controllers/equipment.controller';
 import { EquipmentService } from './services/equipment.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Equipment } from './models/euipment.model';
+import { Equipment } from './models/equipment.model';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Equipment])],

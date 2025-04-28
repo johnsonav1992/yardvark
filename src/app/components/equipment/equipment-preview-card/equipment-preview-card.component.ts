5bf6511140e8a56738aac205bdb2453f748cb4e3
Alex Johnson <johnsonav1992@gmail.com>
2025-04-28 15:01:28 -0500

    feat: enhance EquipmentPreviewCard layout and functionality with maintenance date display

--- src/app/components/equipment/equipment-preview-card/equipment-preview-card.component.ts
+++ src/app/components/equipment/equipment-preview-card/equipment-preview-card.component.ts
@@ -1,10 +1,12 @@
 import { Component, input } from '@angular/core';
 import { CardModule } from 'primeng/card';
 import { Equipment } from '../../../types/equipment.types';
+import { DatePipe } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'equipment-preview-card',
-  imports: [CardModule],
+  imports: [CardModule, DatePipe, ButtonModule],
   templateUrl: './equipment-preview-card.component.html',
   styleUrl: './equipment-preview-card.component.scss'
 })

365418f2a77666e7ff8309f830321b60f78165c8
Alex Johnson <johnsonav1992@gmail.com>
2025-05-12 05:37:25 -0500

    mocking out some analytics

--- src/app/pages/analytics/analytics.component.ts
+++ src/app/pages/analytics/analytics.component.ts
@@ -1,11 +1,80 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { PageContainerComponent } from '../../components/layout/page-container/page-container.component';
+import { CardModule } from 'primeng/card';
+import { ChartData } from 'chart.js';
+import { ChartModule } from 'primeng/chart';
+import { GlobalUiService } from '../../services/global-ui.service';
 
 @Component({
   selector: 'analytics',
-  imports: [],
+  imports: [PageContainerComponent, CardModule, ChartModule],
   templateUrl: './analytics.component.html',
   styleUrl: './analytics.component.scss'
 })
 export class AnalyticsComponent {
+  private _globalUiService = inject(GlobalUiService);
 
+  public isDarkMode = this._globalUiService.isDarkMode;
+
+  public charts = computed(() => {
+    return [
+      {
+        title: 'Mowing Frequency Over Time',
+        chartData: this.mowingChartData(),
+        options: {
+          maintainAspectRatio: false,
+          aspectRatio: 0.75,
+          scales: {
+            y: {
+              beginAtZero: true,
+              grid: this.isDarkMode()
+                ? {
+                    color: 'rgba(200, 200, 200, 0.2)'
+                  }
+                : undefined
+            },
+            x: {
+              grid: this.isDarkMode()
+                ? {
+                    color: 'rgba(200, 200, 200, 0.2)'
+                  }
+                : undefined
+            }
+          },
+          plugins: {
+            legend: {
+              position: 'chartArea',
+              align: 'end'
+            }
+          }
+        }
+      }
+    ];
+  });
+
+  public mowingChartData = computed<ChartData<'line'>>(() => ({
+    labels: [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec'
+    ],
+    datasets: [
+      {
+        type: 'line',
+        label: `Mowing Frequency`,
+        data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+        borderColor: 'red',
+        tension: 0.4
+      }
+    ]
+  }));
 }

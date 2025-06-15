ab2829a870ae7ac701c6b13e31ffe19ec417884e
Alex Johnson <johnsonav1992@gmail.com>
2025-06-15 14:53:06 -0500

    feat: add weather icon constants and update forecast marker logic

---
+++ src/app/constants/weather-constant.ts
@@ -0,0 +1,13 @@
+export const RAIN_CHANCE_MEDIUM_THRESHOLD = 40;
+export const RAIN_CHANCE_LOW_THRESHOLD = 20;
+export const FREEZING_TEMPERATURE_F = 32;
+export const MEDIUM_TEMPERATURE_F = 60;
+export const HIGH_TEMPERATURE_F = 80;
+
+export const WEATHER_ICONS = {
+  snowflake: 'ti ti-snowflake',
+  rain: 'ti ti-cloud-rain',
+  cloud: 'ti ti-cloud',
+  sun: 'ti ti-sun',
+  hot: 'ti ti-temperature-sun'
+};

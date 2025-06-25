6fd8aa9322b0d07dbe34dfe3ff0d3f4c0d3e20f4
Alex Johnson <johnsonav1992@gmail.com>
2025-06-25 05:28:16 -0500

    I don't like this but I'll get back to it later

--- src/app/components/calculators/fertilizer-calculator/fertilizer-calculator.ts
+++ src/app/components/calculators/fertilizer-calculator/fertilizer-calculator.ts
@@ -1,26 +1,63 @@
-import { Component, computed, linkedSignal, signal } from '@angular/core';
-import { getPoundsOfProductForDesiredN } from '../../../utils/lawnCalculatorUtils';
+import { Component, signal } from '@angular/core';
+import {
+  getPoundsOfNInFertilizerApp,
+  getPoundsOfProductForDesiredN
+} from '../../../utils/lawnCalculatorUtils';
+import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'fertilizer-calculator',
-  imports: [],
+  imports: [FormsModule, InputTextModule],
   templateUrl: './fertilizer-calculator.html',
   styleUrl: './fertilizer-calculator.scss'
 })
 export class FertilizerCalculator {
-  private totalLawnSizeSignal = signal<number | null>(null);
-  private poundsOfNSignal = signal<number | null>(null);
-  private nitrogenRateSignal = signal<number | null>(null);
+  public totalLawnSize = signal<number | null>(null);
+  public nitrogenRate = signal<number | null>(null);
 
-  public fertilizerAmount = linkedSignal(() => {
-    const totalLawnSize = this.totalLawnSizeSignal();
-    const poundsOfN = this.poundsOfNSignal();
-    const nitrogenRate = this.nitrogenRateSignal();
+  public poundsOfN = signal<number | null>(null);
+  public fertilizerAmount = signal<number | null>(null);
 
-    return getPoundsOfProductForDesiredN({
-      desiredLbsOfNPer1000SqFt: poundsOfN || 0,
-      guaranteedAnalysisOfProduct: `${nitrogenRate || 0}-0-0`,
-      totalSquareFeet: totalLawnSize || 0
-    });
-  });
+  public onPoundsOfNChange(newPoundsOfN: number | null): void {
+    this.poundsOfN.set(newPoundsOfN);
+
+    if (newPoundsOfN === null) {
+      return this.fertilizerAmount.set(null);
+    }
+
+    const totalLawnSize = this.totalLawnSize();
+    const nitrogenRate = this.nitrogenRate();
+
+    if (totalLawnSize && nitrogenRate) {
+      const calculatedFertilizerAmount = getPoundsOfProductForDesiredN({
+        desiredLbsOfNPer1000SqFt: newPoundsOfN,
+        guaranteedAnalysisOfProduct: `${nitrogenRate}-0-0`,
+        totalSquareFeet: totalLawnSize
+      });
+
+      this.fertilizerAmount.set(calculatedFertilizerAmount);
+    }
+  }
+
+  public onFertilizerAmountChange(newFertilizerAmount: number | null): void {
+    this.fertilizerAmount.set(newFertilizerAmount);
+
+    if (newFertilizerAmount === null) {
+      return this.poundsOfN.set(null);
+    }
+
+    const totalLawnSize = this.totalLawnSize();
+    const nitrogenRate = this.nitrogenRate();
+
+    if (totalLawnSize && nitrogenRate) {
+      const calculatedPoundsOfN = getPoundsOfNInFertilizerApp({
+        poundsOfProduct: newFertilizerAmount,
+        guaranteedAnalysisOfProduct: `${nitrogenRate}-0-0`,
+        totalSquareFeet: totalLawnSize
+      });
+
+      this.poundsOfN.set(calculatedPoundsOfN);
+    }
+  }
 }

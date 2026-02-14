9a92eff186b4fa650088e19f218f8a070091849a
Alex Johnson <johnsonav1992@gmail.com>
2026-02-14 14:42:45 -0600

    Refactor DatabaseTelemetryService to streamline query telemetry wrapping

--- backend/src/db/database-telemetry.service.ts
+++ backend/src/db/database-telemetry.service.ts
@@ -1,45 +1,35 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { DataSource } from 'typeorm';
+import { DataSource, QueryRunner, ReplicationMode } from 'typeorm';
 import { LogHelpers } from '../logger/logger.helpers';
 
 @Injectable()
 export class DatabaseTelemetryService implements OnModuleInit {
   constructor(private dataSource: DataSource) {}
 
-  async onModuleInit() {
-    // Helper function to wrap query methods with telemetry
-    const wrapQueryMethod = (originalFn: Function) => {
-      return async function (this: any, query: string, parameters?: any[]) {
+  onModuleInit() {
+    const originalCreateQueryRunner = this.dataSource.createQueryRunner.bind(
+      this.dataSource,
+    );
+
+    this.dataSource.createQueryRunner = (mode?: ReplicationMode) => {
+      const queryRunner = originalCreateQueryRunner(mode);
+      const originalQuery = queryRunner.query.bind(queryRunner);
+
+      queryRunner.query = async (...args: Parameters<QueryRunner['query']>) => {
         const start = Date.now();
 
         try {
-          const result = await originalFn.call(this, query, parameters);
+          const result = await originalQuery(...args);
           LogHelpers.recordDatabaseCall(Date.now() - start, false);
+
           return result;
         } catch (error) {
           LogHelpers.recordDatabaseCall(Date.now() - start, true);
+
           throw error;
         }
       };
-    };
 
-    // Wrap the DataSource query method
-    const originalQuery = this.dataSource.query.bind(this.dataSource);
-    this.dataSource.query = wrapQueryMethod(originalQuery);
-
-    // Wrap the EntityManager query method
-    const manager = this.dataSource.manager;
-    const originalManagerQuery = manager.query.bind(manager);
-    manager.query = wrapQueryMethod(originalManagerQuery);
-
-    // Intercept query runner creation to wrap individual query runners
-    const originalCreateQueryRunner =
-      this.dataSource.createQueryRunner.bind(this.dataSource);
-
-    this.dataSource.createQueryRunner = function (mode?: 'master' | 'slave') {
-      const queryRunner = originalCreateQueryRunner(mode);
-      const originalRunnerQuery = queryRunner.query.bind(queryRunner);
-      queryRunner.query = wrapQueryMethod(originalRunnerQuery);
       return queryRunner;
     };
   }

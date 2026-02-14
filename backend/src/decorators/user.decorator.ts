c5a41812fadb487fbc3c472a1286c2cdf08a4ca3
Alex Johnson <johnsonav1992@gmail.com>
2026-02-14 07:14:51 -0600

    docs: update usage example in User decorator documentation

--- backend/src/decorators/user.decorator.ts
+++ backend/src/decorators/user.decorator.ts
@@ -4,6 +4,7 @@ import { ExtractedUserRequestData } from '../types/request';
 /**
  * Custom decorator to extract user information from the request object.
  * Usage:
+ * ```ts
  *   @Get('some-endpoint')
  *   someMethod(@User('userId') userId: string) {
  *     // use userId here
@@ -13,6 +14,7 @@ import { ExtractedUserRequestData } from '../types/request';
  *   anotherMethod(@User() user: ExtractedUserRequestData) {
  *     // use user.userId, user.email, user.name here
  *   }
+ * ```
  */
 export const User = createParamDecorator(
   (data: keyof ExtractedUserRequestData | undefined, ctx: ExecutionContext) => {
